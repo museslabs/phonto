94615e5a61a9788b67c2ade7f78ca0e33fb8ef03
ploMP4 <artopouloskon@gmail.com>
2026-05-14 13:47:12 +0300

    feat: add cli interface

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ mod gl_renderer;
 mod phonto;
 mod wayland;
 
+use clap::Parser;
 use phonto::Phonto;
 
-const WALLPAPER_PATH: &str = "/home/plo/dotfiles/wallpapers/animated/night-city.mp4";
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the video file
+    path: String,
+}
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+    let args = Args::parse();
     let mut phonto = Phonto::new()?;
-    phonto.play(String::from(WALLPAPER_PATH))
+    phonto.play(String::from(args.path))
 }
